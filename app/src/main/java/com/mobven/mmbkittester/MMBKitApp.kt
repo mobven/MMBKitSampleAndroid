095eeb3eb6da1532fba839ea043532c63a95e728
Erel ÖZÇAKIRLAR <erel.ozcakirlar@mobven.com>
2020-03-26 15:53:21 +0300

    Initial commit

---
+++ app/src/main/java/com/mobven/mmbkittester/MMBKitApp.kt
@@ -0,0 +1,14 @@
+package com.mobven.mmbkittester
+
+import android.app.Application
+import com.mobven.accountsecurity.AccountSecurity
+import com.mobven.appsecurity.AppSecurity
+import com.mobven.core.MMBKit
+
+class MMBKitApp: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        MMBKit.init(this, AppSecurity, AccountSecurity)
+    }
+}
